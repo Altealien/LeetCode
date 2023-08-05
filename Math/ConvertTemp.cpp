989841ec83372eb6f8b784f6cea1453895f8884d
Altealien <omotayodavid657@gmail.com>
2023-08-05 19:22:58 +0100

    Initial Commit

---
+++ Math/ConvertTemp.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+ #include <vector>
+ #include <iomanip>
+ 
+ std::vector<double> convertTemperature(double celsius) {
+        double toKelvin = celsius + 273.15;
+        double toFahrenheit = (celsius*1.80)+32.00;
+        std::vector<double> converted ={toKelvin,toFahrenheit};
+        return converted;
+    }
+
+int main(){
+   std::vector<double> converted = convertTemperature(30.00);
+    for (double digit :converted){
+        std::cout << digit << std::endl;
+    }
+}
