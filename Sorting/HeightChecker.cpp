3dbf146c77fcab9fdcff172e934d9fa40475c148
Altealien <omotayodavid657@gmail.com>
2023-08-11 03:28:12 +0100

    Initial Commit

---
+++ Sorting/HeightChecker.cpp
@@ -0,0 +1,26 @@
+/*1051. Height Checker
+
+A school is trying to take an annual photo of all the students. T
+he students are asked to stand in a single file line in non-decreasing order by height. 
+Let this ordering be represented by the integer array expected where expected[i] is the expected height 
+of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. 
+Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].*/
+
+#include <bits/stdc++.h>
+
+int heightChecker(std::vector<int>& heights) {
+    int count{0};
+    std::vector<int> expected = heights;
+    sort(expected.begin(),expected.end());
+
+     for(int i =0; i< heights.size(); i++){
+        if(heights[i]!= expected[i]){
+            count++;
+         }
+    }
+    return count;
+}
